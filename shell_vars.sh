b2349fdb39c51354444e6829cc037ba44ca995d5
Matthew Rice <mattricesound@gmail.com>
2023-02-14 23:47:55 +0000

    Fix output root shell var

--- shell_vars.sh
+++ shell_vars.sh
@@ -1,4 +1,4 @@
 export DATASET_ROOT="./data/VocalSet"
-export DATASET_ROOT="/scratch/VocalSet/processed"
+export OUTPUT_ROOT="/scratch/VocalSet/processed"
 export WANDB_PROJECT="RemFX"
 export WANDB_ENTITY="mattricesound"
